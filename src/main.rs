0c971127d4d399e090b4e4f93c60cbdba4670877
Antti Kytö <antti.kytoe@gmail.com>
2020-06-25 09:27:51 +0300

    first commit

---
+++ src/main.rs
@@ -0,0 +1,95 @@
+#[cfg(windows)] extern crate winapi;
+use std::io::Error;
+use std::mem;
+use std::ptr::null_mut;
+use winapi::shared::windef::HWND;
+use winapi::um::winuser::{
+    MSG,
+    DispatchMessageW,
+    TranslateMessage,
+    GetMessageW,
+};
+
+fn win32_string( value : &str ) -> Vec<u16> {
+    use std::ffi::OsStr;
+    use std::iter::once;
+    use std::os::windows::ffi::OsStrExt;
+    OsStr::new( value ).encode_wide().chain( once( 0 ) ).collect()
+}
+
+struct Window {
+    handle : HWND,
+}
+
+fn handle_message( window : &mut Window ) -> bool {
+    unsafe {
+        let mut message : MSG = mem::zeroed();
+        if GetMessageW( &mut message as *mut MSG, window.handle, 0, 0 ) > 0 {
+            TranslateMessage( &message as *const MSG );
+            DispatchMessageW( &message as *const MSG );
+            true
+        } else {
+            false
+        }
+    }
+}
+
+fn create_window( name: &str, title: &str) -> Result<Window, Error>{
+    use winapi::um::winuser::{CW_USEDEFAULT, WS_VISIBLE, WS_OVERLAPPEDWINDOW, CreateWindowExW, DefWindowProcW, WNDCLASSW, CS_HREDRAW, CS_VREDRAW, RegisterClassW};
+
+    //Convert strings to correct format
+    let name = win32_string( name );
+    let title = win32_string( title );
+
+    unsafe {
+        let hinstance = winapi::um::libloaderapi::GetModuleHandleW( null_mut() );   
+        let wnd_class = WNDCLASSW {
+            style : CS_HREDRAW | CS_VREDRAW,
+            lpfnWndProc : Some( DefWindowProcW ),
+            hInstance : hinstance,
+            lpszClassName : name.as_ptr(),
+            cbClsExtra : 0,
+            cbWndExtra : 0,
+            hIcon: null_mut(),
+            hCursor: null_mut(),
+            hbrBackground: null_mut(),
+            lpszMenuName: null_mut(),
+        };
+
+        RegisterClassW( &wnd_class );
+        
+        let handle = CreateWindowExW(
+            0,
+            name.as_ptr(),
+            title.as_ptr(),
+            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            CW_USEDEFAULT,
+            null_mut(),
+            null_mut(),
+            hinstance,
+            null_mut() );
+
+        if handle.is_null() {
+            Err( Error::last_os_error() )
+        } else {
+            Ok( Window { handle } )
+        }
+    }
+}
+
+fn main() {
+    let name = "winclass1";
+    let title = "muntitle";
+
+    let mut window = create_window(&name, &title).unwrap();
+
+    loop {
+        if !handle_message( &mut window ) {
+            break;
+        }
+    }
+    
+}
